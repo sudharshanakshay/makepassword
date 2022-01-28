37d6644cd0711291f14b6f0754d6f517cc3eacbe
sudharshan <sudharshan6acharya@gmail.com>
2022-01-28 17:23:33 +0530

    activity body

--- app/src/main/java/com/example/makepassword/MainActivity.java
+++ app/src/main/java/com/example/makepassword/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,7 +18,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     Random random =  new Random();
-    Switch includeSpecial, includeUpperCase, includeLowerCase, includeNumericals, includeAmbiguous;
+    Switch includeSpecial, includeUpperCase, includeLowerCase, includeNumerals, includeAmbiguous;
 
 
     private char getMyPassword(int type){
@@ -34,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 break;
             case 2:
-                if(includeNumericals.isChecked())
+                if(includeNumerals.isChecked())
                 p = (char)(random.nextInt(9) + 48); // random number
             break;
             case 3:
@@ -49,9 +50,9 @@ public class MainActivity extends AppCompatActivity {
         return p;
     }
 
-    private void setAtleastOneVarientON(){
+    private void setVarientON(){
         if(includeSpecial.isChecked() || includeUpperCase.isChecked() || includeLowerCase.isChecked()
-        || includeNumericals.isChecked() || includeAmbiguous.isChecked()){
+        || includeNumerals.isChecked() || includeAmbiguous.isChecked()){
             return;
         }else {
             includeLowerCase.setChecked(true);
@@ -60,12 +61,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-    private String generatepass(){
+    private String generatepass(int passLEN){
         StringBuilder password= new StringBuilder();
         char value;
-        while(password.length() <= 8){
-            setAtleastOneVarientON();
+        while(password.length() <= passLEN){
+            setVarientON();
             value = getMyPassword(random.nextInt(5));
             if(value == ' ') continue;
             password.append(value);
@@ -77,28 +77,50 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_body);
 
         Button genPass = findViewById(R.id.genpass);
+        Button copyBtn = findViewById(R.id.copyBtn);
         final TextView displayPass = findViewById(R.id.displayPass);
-
+        final TextView displayPassLen = findViewById(R.id.displayPassLen);
+        SeekBar getPassLen = findViewById(R.id.getPassLenSeekBar);
         includeSpecial = findViewById(R.id.includeSpecial);
         includeUpperCase = findViewById(R.id.includeUpperChar);
         includeLowerCase = findViewById(R.id.includeLowerCase);
-        includeNumericals = findViewById(R.id.includeNumericals);
+        includeNumerals = findViewById(R.id.includeNumericals);
         includeAmbiguous = findViewById(R.id.includeAmbiguous);
         includeLowerCase.setChecked(true);
         includeUpperCase.setChecked(true);
 
-        Button copyBtn = findViewById(R.id.copyBtn);
-
         final String[] password = new String[1];
         final String lable = "pass";
+        final int[] passLen = new int[1];
+        passLen[0] = 8;
+
+        displayPass.setText(generatepass(passLen[0]));
+
+
+        getPassLen.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                passLen[0] =  progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                displayPassLen.setText(passLen.toString());
+            }
+        });
 
         genPass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                password[0] = generatepass();
+                password[0] = generatepass(passLen[0]);
                 displayPass.setText(password[0]);
             }
         });
